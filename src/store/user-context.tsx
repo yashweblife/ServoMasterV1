04ed228a598e0f1fe2b788dd0131b553433affef
yashweblife <yashweblife20@gmail.com>
2022-10-03 12:14:20 -0500

    Added Delete User

--- src/store/user-context.tsx
+++ src/store/user-context.tsx
@@ -6,7 +6,7 @@ import {
   signInWithEmailAndPassword,
   signOut,
 } from "firebase/auth";
-import { addDoc, collection, doc, setDoc } from "firebase/firestore";
+import { addDoc, collection, deleteDoc, doc, DocumentData, getDocs, QuerySnapshot, setDoc } from "firebase/firestore";
 import { createContext, useEffect, useState } from "react";
 import { auth, db, makeRandString } from "../utils/utils";
 
@@ -36,13 +36,16 @@ export const UserContextProvider = (props: any) => {
   }, []);
 
   const loginUser = (email: string, pass: string) => {
-    console.log(email);
     signInWithEmailAndPassword(auth, email, pass)
       .then(() => {
         console.log("Logged In");
       })
       .catch((err: Error) => {
         console.log(err.message);
+        alert({
+          header:err.message,
+          buttons:[{text:"Confirm"}]
+        })
       });
   };
   const logoutUser = () => {
@@ -52,6 +55,17 @@ export const UserContextProvider = (props: any) => {
     if (email == "") {
       alert({
         header: "Enter An Email",
+        buttons: [
+          {
+            text: "Confirm",
+          },
+        ],
+      });
+      return;
+    }
+    if(!email.includes("@")){
+      alert({
+        header: "Enter a valid email",
         buttons: [
           {
             text: "Confirm",
@@ -73,7 +87,7 @@ export const UserContextProvider = (props: any) => {
     }
     if(pass.length<8){
       alert({
-        header: "Enter A Password",
+        header: "Password is too short (atleast 8 characters)",
         buttons: [
           {
             text: "Confirm",
@@ -119,7 +133,29 @@ export const UserContextProvider = (props: any) => {
   const editUser = () => {};
   const deleteCurrentUser = () => {
     if (auth.currentUser) {
-      deleteUser(auth.currentUser);
+      const ref = doc(db,"users",auth.currentUser.uid)
+      const pref = collection(ref,"project_list")
+      const dref = collection(ref,"device_list")
+
+      getDocs(pref).then((snap:QuerySnapshot)=>{
+        if(!snap.empty){
+          snap.docs.forEach((d:DocumentData)=>{
+            deleteDoc(doc(pref, d.id))
+          })
+        }
+      }).then(()=>{
+        getDocs(dref).then((snap:QuerySnapshot)=>{
+          if(!snap.empty){
+            snap.docs.forEach((d:DocumentData)=>{
+              deleteDoc(doc(dref, d.id))
+            })
+          }
+        }).then(()=>{
+          if(auth.currentUser){
+            deleteUser(auth.currentUser)
+          }
+        })
+      })
     }
   };
 
