aa44381596abcd3d86810a89304328a45349a077
yashweblife <yashweblife20@gmail.com>
2022-10-20 14:01:43 -0500

    Normalized login page style

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,12 +1,10 @@
 import { IonContent, IonPage } from "@ionic/react";
-import { useContext, useState } from "react";
+import { useState } from "react";
 import LoginComponent from "../components/LoginComponent";
 import SignupComponent from "../components/SignupComponent";
-import { UserContext } from "../store/user-context";
 import "./LoginPage.scss";
 const LoginPage: React.FC = () => {
   const [isLogin, setIsLogin] = useState<Boolean>(true);
-  const userContext = useContext(UserContext);
   return (
     <IonPage>
       <IonContent className="authPage" fullscreen>
