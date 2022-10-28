301024ced2a9624709bf9e5e84a6fad1b38c8c0f
yashweblife <yashweblife20@gmail.com>
2022-10-28 17:24:00 -0500

    Added some content

--- src/pages/TutorialsPage.tsx
+++ src/pages/TutorialsPage.tsx
@@ -1,5 +1,52 @@
+import { IonCard, IonCardContent, IonCardHeader, IonContent, IonPage, IonTitle, IonToolbar } from "@ionic/react"
+
 const TutorialsPage:React.FC = ()=>{
-    return(<></>)
+    return(
+    <IonPage>
+        <IonContent>
+            <IonCard>
+                <IonCardHeader>
+                    <IonToolbar>
+                        <IonTitle>Adding A Device</IonTitle>
+                    </IonToolbar>
+                </IonCardHeader>
+                <IonCardContent>
+                    Learn how to add your ServoMaster Blox to your projects.
+                </IonCardContent>
+            </IonCard>
+            <IonCard>
+                <IonCardHeader>
+                    <IonToolbar>
+                        <IonTitle>Adding a Project</IonTitle>
+                    </IonToolbar>
+                </IonCardHeader>
+                <IonCardContent>
+                    Learn how to add a project.
+                </IonCardContent>
+            </IonCard>
+            <IonCard>
+                <IonCardHeader>
+                    <IonToolbar>
+                        <IonTitle>Edit your projects</IonTitle>
+                    </IonToolbar>
+                </IonCardHeader>
+                <IonCardContent>
+                    Learn what you can do with your projects.
+                </IonCardContent>
+            </IonCard>
+            <IonCard>
+                <IonCardHeader>
+                    <IonToolbar>
+                        <IonTitle>Tips and Tricks</IonTitle>
+                    </IonToolbar>
+                </IonCardHeader>
+                <IonCardContent>
+                    Learn some cool things you can do.
+                </IonCardContent>
+            </IonCard>
+        </IonContent>
+    </IonPage>
+    )
 }
 
 export default TutorialsPage
