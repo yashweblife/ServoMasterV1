57a0cc959e376fb25ae68ed96e39ac5ba87318e8
yashweblife <yashweblife20@gmail.com>
2022-10-28 17:33:34 -0500

    Comments

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -3,7 +3,14 @@ import EditorComponent from "../components/EditorComponent";
 import ProjectListComponent from "../components/ProjectListComponent";
 import { ProjectListContext } from "../store/project-list-context";
 import "./Tab1.scss";
-
+/**
+ * ### Tab1 | Project List/Editor
+ * This tab contains the project list and the editor.
+ * It initially allows you to add projects.
+ * You can open the projects in the editor.
+ * The editor is where you can perform mutations to the projects.
+ * @returns 
+ */
 const Tab1: React.FC = () => {
   const projectListContext = useContext(ProjectListContext);
 
