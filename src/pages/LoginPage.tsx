48d025772b442d54687877d7a421b2d09d416034
yashweblife <yashweblife20@gmail.com>
2022-10-28 17:24:12 -0500

    Added comments

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -3,6 +3,14 @@ import { useState } from "react";
 import LoginComponent from "../components/LoginComponent";
 import SignupComponent from "../components/SignupComponent";
 import "./LoginPage.scss";
+/**
+ * ### LoginPage
+ * This page handles user authentication.
+ * It has the login component and the signup component
+ * TODO: Add proper error management
+ * TODO: Redesign the login page view
+ * @returns 
+ */
 const LoginPage: React.FC = () => {
   const [isLogin, setIsLogin] = useState<Boolean>(true);
   return (
